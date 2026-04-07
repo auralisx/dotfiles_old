054a4bc6f364079e2e246a78599237b08971277f
auralisx <sujan.koju111@gmail.com>
2026-04-07 13:55:47 +0545

    FISH: added wrapper for changing directory in yazi

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -53,6 +53,16 @@ set fish_cursor_visual block
 
 ### FUNCTIONS ###
 
+# y shell wrapper with ability to change directory after yazi exits
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
+
 ### END OF FUNCTIONS ###
 
 
